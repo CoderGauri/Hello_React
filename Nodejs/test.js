698bbb4e9b589748a37649bc61880f01af01939b
Gauri <gauriM304@gmail.com>
2024-05-29 10:31:30 +0530

    multiple functions added

--- Nodejs/test.js
+++ Nodejs/test.js
@@ -1,3 +1,6 @@
 const calculator = require("./calculator");
 
-calculator.addition(3,4)
+calculator.addition(3,4);
+calculator.substraction(3,4);
+calculator.multiplication(3,5);
+calculator.division(3,4);
