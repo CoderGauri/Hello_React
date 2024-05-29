0ac2db31af6ac01a0724544c527591967c81ebba
Gauri <gauriM304@gmail.com>
2024-05-29 11:03:54 +0530

    child process

---
+++ Nodejs/node_module_system/1_cp.js
@@ -0,0 +1,6 @@
+const cp = require('child_process');
+
+//cp.execSync('calc');
+//cp.execSync('start chrome https://www.scaler.com/academy/mentee-dashboard/');
+
+console.log('ouput' + cp.execSync('node demo.js'))
