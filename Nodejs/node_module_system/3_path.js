b39d1636e2925c8791bf8d19da1cc2d717f8d1fa
Gauri <gauriM304@gmail.com>
2024-05-29 11:33:19 +0530

    path module

---
+++ Nodejs/node_module_system/3_path.js
@@ -0,0 +1,8 @@
+const path = require('path');
+
+let ext = path.extname('D:\Learning\github-projects\Github-new\Hello_React\Nodejs\node_module_system\f1.txt');
+console.log(ext);
+let baseName = path.basename('D:\\Learning\\github-projects\\Github-new\\Hello_React\\Nodejs\\node_module_system\\f1.txt')
+console.log(baseName);
+console.log(__filename);
+console.log(__dirname);
