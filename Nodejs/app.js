ad3742b367bd214517cc4dcc8c6d37cb01710911
Gauri <gauriM304@gmail.com>
2024-05-29 10:07:21 +0530

    node js lesson 1

---
+++ Nodejs/app.js
@@ -0,0 +1,12 @@
+// console.log("hello world");
+
+// function greet(){
+//     console.log("good morning");
+// }
+
+// greet();
+
+console.log(global);
+
+let name = "gauri";
+console.log(global.name); //undefined
